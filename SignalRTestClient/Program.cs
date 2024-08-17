ac548727a1b4ac72ad4c64a2672e3e7bd4bd581e
Hasan Çoban <hasancoban13579@gmail.com>
2024-08-17 15:56:48 +0300

    Create chat hubs and a repository for client users to test the chat functionality

---
+++ SignalRTestClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5223/chatHub", options =>
+    {
+        options.HttpMessageHandlerFactory = (message) =>
+        {
+            if (message is HttpClientHandler clientHandler)
+                clientHandler.ServerCertificateCustomValidationCallback +=
+                    (sender, certificate, chain, sslPolicyErrors) => { return true; };
+            return message;
+        };
+    })
+    .WithAutomaticReconnect()
+    .Build();
+
+connection.On<string, string>("ReceiveMessage", (user, message) => 
+{
+    Console.WriteLine($"{user}: {message}");
+});
+
+try
+{   
+    Console.WriteLine("Please enter your name: ");
+    var userName = Console.ReadLine();
+    
+    await connection.StartAsync();
+    Console.WriteLine("Connection started");
+
+    while (true)
+    {
+        var userInput = Console.ReadLine();
+        await connection.SendAsync("SendMessage", userName, userInput);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
