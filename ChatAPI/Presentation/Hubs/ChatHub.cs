170f7e3a6f9f5174acc06a052fe337deed711ebb
Hasan Çoban <hasancoban13579@gmail.com>
2024-08-20 17:51:18 +0300

    Create a method for sending private messages between users

--- ChatAPI/Presentation/Hubs/ChatHub.cs
+++ ChatAPI/Presentation/Hubs/ChatHub.cs
@@ -1,11 +1,46 @@
+using System.Collections.Concurrent;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatAPI.Presentation.Hubs;
 
 public class ChatHub : Hub
 {
-  public async Task SendMessage(string user, string message)
+  private static readonly ConcurrentDictionary<string, string> Users = new();
+  public async Task SendMessage(string message)
   {
-    await Clients.All.SendAsync("ReceiveMessage", user, message);
+    Console.WriteLine(Context.UserIdentifier);
+    Console.WriteLine("message sent");
+
+    await Clients.All.SendAsync("ReceiveMessage", Users[Context.ConnectionId], message);
+  }
+
+  public bool SetUserId(string userId)
+  {
+    try
+    {
+      Users[Context.ConnectionId] = userId;
+      return true;
+    }
+    catch (System.Exception)
+    {
+      return false;
+    }
+  }
+
+  public async Task SendPrivateMessage(string receiverUserId, string message)
+  {
+    var receiverConnectionId =
+      Users.FirstOrDefault(u => u.Value == receiverUserId).Key;
+
+    if (receiverConnectionId != null)
+    {
+      await Clients.Client(receiverConnectionId)
+        .SendAsync("ReceiveMessage", Users[Context.ConnectionId], message);
+    }
+  }
+
+  public override Task OnConnectedAsync()
+  {
+    return base.OnConnectedAsync();
   }
 }
--- SignalRTestClient/Program.cs
+++ SignalRTestClient/Program.cs
@@ -14,23 +14,38 @@ var connection = new HubConnectionBuilder()
     .WithAutomaticReconnect()
     .Build();
 
-connection.On<string, string>("ReceiveMessage", (user, message) => 
+connection.On<string, string>("ReceiveMessage", (user, message) =>
 {
     Console.WriteLine($"{user}: {message}");
 });
 
 try
-{   
-    Console.WriteLine("Please enter your name: ");
-    var userName = Console.ReadLine();
-    
+{
     await connection.StartAsync();
     Console.WriteLine("Connection started");
 
+    Console.WriteLine("Please enter your name: ");
+    var userName = Console.ReadLine();
+    await connection.InvokeAsync("SetUserId", userName);
+
+
     while (true)
     {
-        var userInput = Console.ReadLine();
-        await connection.SendAsync("SendMessage", userName, userInput);
+        Console.WriteLine("Who do you want to send a message? Press enter if you want to send to everyone, otherwise enter the user name you want to send a message to.");
+        var receiverUserId = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(receiverUserId))
+        {
+            Console.WriteLine($"Sending message to {receiverUserId}: ");
+            var message = Console.ReadLine();
+            await connection.InvokeAsync("SendPrivateMessage", receiverUserId, message);
+        }
+        else
+        {
+            Console.WriteLine("Sending message to everyone: ");
+            var message = Console.ReadLine();
+            await connection.InvokeAsync("SendMessage", message);
+        }
     }
 }
 catch (Exception ex)
